9888b18ef5a52e3393697c702dc3c1c477cef339
GimhanD7 <gimhandeshapriya567@gmail.com>
2022-12-02 20:57:40 +0530

    Add files via upload

---
+++ gimhan/src/main/java/com/DeleteCustomerServlet.java
@@ -0,0 +1,42 @@
+package com;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/DeleteCustomerServlet")
+public class DeleteCustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("cusid");
+		boolean isTrue;
+		
+		isTrue = CustomerDBUtil.deleteCustomer(id);
+		
+		if (isTrue == true) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("customerinsert.jsp");
+			dispatcher.forward(request, response);
+		}
+		else {
+			
+			List<Customer> cusDetails = CustomerDBUtil.getCustomerDetails(id);
+			request.setAttribute("cusDetails", cusDetails);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("useraccount.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		
+		
+	}
+
+}
